1cd9fbcfb44aa679f28b9d79a73d7009f6a8d80c
Mathias <tympanix@gmail.com>
2018-01-31 08:53:15 +0100

    Fix priority issue with ngIf

--- dist/angular-table-resize.js
+++ dist/angular-table-resize.js
@@ -288,6 +288,7 @@ angular.module("rzTable").directive('rzCol', [function() {
   // Return this directive as a object literal
   return {
     restrict: 'A',
+    priority: 650, /* before ng-if */
     link: link,
     require: '^^rzTable',
     scope: true
